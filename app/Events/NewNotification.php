c008054e61c45bce46992d93487f69b744299f2e
keroles <keroles.nabil.official@gmail.com>
2022-06-25 20:28:24 +0200

    notification pusher

---
+++ app/Events/NewNotification.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Events;
+
+use Carbon\Carbon;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class NewNotification implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public $user_id;
+    public $user_name;
+    public $post_id;
+    public $comment;
+    public $data;
+    public $time;
+    public function __construct($data)
+    {
+
+        $this->user_id=$data['user_id'];
+        $this->user_name=$data['user_name'];
+        $this->post_id=$data['post_id'];
+        $this->comment=$data['comment'];
+        $this->date = date("Y-m-d", strtotime(Carbon::now()));
+        $this->time = date("h:i A", strtotime(Carbon::now()));
+
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+//        return new Channel('new-notification');
+
+        return ['new-notification'];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('comment', 'HomeController@saveComment')->name('comment.save');
+
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
